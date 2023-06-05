ea98e9665beb2de2edd9a00242c78abb2f3abada
Aaron <xiroxhexi@gmail.com>
2023-06-05 15:13:27 +0100

    commit utf8 2

--- models/follow.js
+++ models/follow.js
@@ -3,16 +3,7 @@
 var mongoose = require('mongoose');
 var Schema= mongoose.Schema;
 
-/*var PublicationSchema = Schema({
 
-        text: String,
-        file: String,
-        create_at: String,
-        user: {type: Schema.ObjectId,ref: 'User'}
-        
-
-
-});*/
 
 const FollowSchema = Schema({
     user: { type: Schema.ObjectId, ref: 'User' },
